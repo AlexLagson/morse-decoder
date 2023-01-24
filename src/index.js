3571242a1c2e65ba4833bb248dcbc7b9f50e3bd8
Alex <alexlagwick12@gmail.com>
2023-01-24 14:20:04 +0100

    init: opload solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,39 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+     let counter = 0;
+    let arr = [];
+    let str = '';
+   
+
+    for (let char of expr) {
+        str += char;
+        counter++;
+        if (counter === 10) {
+            arr.push(str)
+            str = '';
+            counter = 0;
+        }
+    }
+
+    return arr.map(item => {
+        let finalLetter = '';
+        if (item === '**********') {
+            finalLetter = ' ';
+            return finalLetter;
+        }
+        for (let i = 0; i <= item.length; i = i + 2) {
+            if (item[i] === '1' && item[i + 1] === '1') finalLetter += '-';
+            if (item[i] === '1' && item[i + 1] === '0') finalLetter += '.';
+        }
+        return finalLetter;
+    }).map(finalLetter => {
+        if (finalLetter === ' ') return ' ';
+        if (finalLetter in MORSE_TABLE) {
+            return MORSE_TABLE[finalLetter]
+        }
+    }).join('');
+
 }
 
 module.exports = {
